179f64627218471ef72ee8dfdfc520daacb02cce
Bismus <alexeybismus@gmail.com>
2026-02-10 22:59:57 +0300

    feat: add logo page

--- lib/pages/logo_page.dart
+++ lib/pages/logo_page.dart
@@ -1,5 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logo_app/cubits/logo_cubit.dart';
+import 'package:logo_app/injection_container.dart';
+import 'package:logo_app/widgets/logo.dart';
 
 @RoutePage()
 class LogoPage extends StatelessWidget {
@@ -7,6 +11,23 @@ class LogoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (_) => sl<LogoCubit>(),
+      child: BlocBuilder<LogoCubit, LogoState>(
+        builder: (context, state) {
+          return Center(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: Logo(
+                isLeftActive: state.isLeftActive,
+                isRightActive: state.isRightActive,
+                onTap: context.read<LogoCubit>().toggleHalfLogo,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
