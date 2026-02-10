20ff011b8ef83edbd78115f53a1f8e2f01027b50
Bismus <alexeybismus@gmail.com>
2026-02-10 22:53:44 +0300

    wip: add constants

---
+++ lib/config/constants/constants.dart
@@ -0,0 +1,4 @@
+const String leftLogoInactivePath = 'assets/left_logo_inactive.svg';
+const String rightLogoInactivePath = 'assets/right_logo_inactive.svg';
+const String leftLogoActivePath = 'assets/left_logo_active.svg';
+const String rightLogoActivePath = 'assets/right_logo_active.svg';
