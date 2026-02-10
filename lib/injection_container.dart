f200b1319197b9ac28f9fbfbbcd4a3f6f6c9d1cf
Bismus <alexeybismus@gmail.com>
2026-02-11 01:23:17 +0300

    wip: register counter cubit in di

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,13 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:logo_app/core/router/router.dart';
+import 'package:logo_app/cubits/counter_cubit.dart';
 import 'package:logo_app/cubits/logo_cubit.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  //! Config
   sl.registerSingleton<AppRouter>(AppRouter());
-
-  //!Cubits
   sl.registerFactory<LogoCubit>(() => LogoCubit());
+  sl.registerFactory<CounterCubit>(() => CounterCubit());
 }
